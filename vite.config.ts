420e71f63ba73729892f5fba4894b78ade537ed7
MPHO MOHOJE <mohoje2mpho@gmail.com>
2025-12-19 02:20:33 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/four4food-revive/",
   server: {
     host: "::",
     port: 8080,
